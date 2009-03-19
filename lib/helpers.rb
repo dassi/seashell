d27269c7103eebb66ebed2512e08252d7e332d4c
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2009-03-19 17:06:56 +0100

    Added comments for helper.rb file

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,22 @@
+#
+# SeaShell
+#
+# Capistrano deployment recipes for seaside platforms
+#
+# Author: Andreas Brodbeck, mindclue gmbh, www.mindclue.ch
+# Licence: MIT (see LICENSE file)
+#
+
+#
+# Some general helper methods
+#
+
+# Show the +message+ in the capistrano trace output
 def trace(message)
   logger.trace(message) if logger
 end
 
+# Show the +message+ on the shell output, highlighted with a color to be important.
 def say(message)
   Capistrano::CLI.ui.say("<%= color('#{message}', :green) %>")
 end
