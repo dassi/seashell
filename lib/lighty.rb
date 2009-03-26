1d14858f9ec08dbcafe93d129dbca9b020ac7063
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2009-03-26 21:54:35 +0100

    Refined lighty tasks, allowing for multiple domains per application.

--- lib/lighty.rb
+++ lib/lighty.rb
@@ -31,29 +31,42 @@ namespace :lighty do
   desc 'Creates the lighty configuration files on the server'
   task :setup do
 
+    # How do I hide seasides paths from the frontend url with lighty?
+    # url.rewrite-once = ( "^(.*)" => "/seaside/#{application}$1" )
+    # url.rewrite-once = ( "^([^\\?]*)(\\?(.*))?" => "/seaside/#{application}/$2" )
+    
     # Beginning of configuration
-    lighty_config = <<-TEXT
+    lighty_config = ''
+    for domain in domains
+      lighty_config << <<-TEXT
 $HTTP["host"] == "#{domain}" {
-  server.document-root = "#{path_application}/web_root"
-  #server.errorlog = "#{path_application}/logs/lighty_error_log"
-  #accesslog.filename = "#{path_application}/logs/access_log"
-  fastcgi.server = ( "/seaside" => (
+  $HTTP["url"] =~ "^/seaside/files/" { 
+    server.document-root = "#{path_web_root}/"
+  } else $HTTP["url"] =~ "^/seaside" { 
+    server.document-root = "#{path_web_root}/"
+    #server.errorlog = "#{path_application}/logs/lighty_error_log"
+    #accesslog.filename = "#{path_application}/logs/access_log"
+    fastcgi.server = ( "/seaside" => (
 TEXT
 
-    # Add a line for each gem
-    gem_configs = []
-    for port in gem_ports
-      gem_configs << "( \"host\" => \"127.0.0.1\", \"port\" => #{port}, \"check-local\" => \"disable\")"
-    end
-    lighty_config << gem_configs.join(",\n") << "\n"
 
-    # End of configuration
-    lighty_config << <<-TEXT
+      # Add a line for each gem
+      gem_configs = []
+      for port in gem_ports
+        gem_configs << "( \"host\" => \"127.0.0.1\", \"port\" => #{port}, \"check-local\" => \"disable\", \"mode\" => \"responder\")"
+      end
+      lighty_config << gem_configs.join(",\n") << "\n"
+
+      # End of configuration
+      lighty_config << <<-TEXT
+      )
     )
-  )
+  }
 }
 TEXT
-
+    end
+    
     put lighty_config, "#{path_lighty_application_configs}/#{application}.conf"
+    say "Don't forget to restart lighttpd"
   end
 end
