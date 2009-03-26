4542570a149a42a7be03c16c3ff00a6ac6b452af
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2009-03-26 22:04:20 +0100

    Improved project config file. Multi domains, entry points.

--- config.rb
+++ config.rb
@@ -8,17 +8,17 @@
 #                                          
 
 ###################################################################################################
-# Configuration (Edit for your project!)
+# Project configuration (Change for your needs)
 #
 
-# Name of the application (No whitespace!)
-set :application, 'mySeasideApp'
+# Name of the application (No whitespace, no special characters!)
+set :application, 'myapp'
 
-# Domain of the deployed application (Example: www.myhost.com)
-set :domain, 'www.myseasideapp.com'
+# Domains of the deployed application (Example: www.myhost.com)
+set :domains, ['myapp.org', 'myapp.com', 'myapp.yourdomain.net']
 
 # Hostname of your server. Your deployed application runs there.
-set :host, 'myseasideapp.com'
+set :host, 'server7'
 
 # User and group which is used for logging into the server
 set :user, 'glass'
@@ -30,31 +30,25 @@ set :path_gemstone, "#{path_base}/product"
 set :path_seaside, "#{path_gemstone}/seaside"
 set :path_lighty_application_configs, '/etc/lighttpd/seaside_applications'
 
-# User and password for Gemstone
+# User and password for logging into Gemstone
 set :gemstone_user, 'DataCurator'
 set :gemstone_password do
-  # Capistrano::CLI.password_prompt("Password for gemstone user #{gemstone_user}: ")
-  Capistrano::CLI.ui.ask("Password for gemstone user #{gemstone_user}: ") do |q|
-    q.echo = false
-    q.default = "swordfish"
-  end
+  ask_password("Password for Gemstone user '#{gemstone_user}': ", 'swordfish')
 end
 
-# Monticello repository
-set :monticello_repository_url, 'http://www.yourserver.com/mySeasideApp'
+# Monticello repository of your project's code
+set :monticello_repository_url, 'http://monticello.somedomain.org/myapp'
+set :monticello_package_name, 'MPMyApp'
 set :monticello_repository_user, 'squeak'
 set :monticello_repository_password do
-  # Capistrano::CLI.password_prompt("Password for Monticello user #{monticello_repository_user}: ")
-  Capistrano::CLI.ui.ask("Password for Monticello user #{monticello_repository_user}: ") do |q|
-    q.echo = false
-    q.default = "seaside"
-  end
+  ask_password("Password for Monticello user '#{monticello_repository_user}': ", 'fh3ss8f382nf')
 end
 
-# Configuration of gems, running one application instance each
+# Configuration of gems, running one application instance each.
 # (Don't forget to renew the web server configuration, if you change this!)
-set :gems_start_port, 9001
+set :gems_start_port, 10001
 set :gems_count, 3
 
-# Configure your seaside entry points here, as a hash with {YourRootComponent => 'entry-point-name', ...}
-set :entry_points, {'VGApplicationComponent' => 'vegl'}
+# Configure your seaside entry points here, as a hash with {YourRootComponent => 'entry-point-name', ...}.
+# Your application will be reachable at /seaside/entry-point-name
+set :entry_points, {'MPApplicationComponent' => 'mywonderfulapp'}
