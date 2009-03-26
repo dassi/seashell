8f0c63c0cdae57a81ad98d0b1934181511c85dfb
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2009-03-26 21:48:40 +0100

    Moved generic part of configuration file into lib.

--- config.rb
+++ config.rb
@@ -56,40 +56,5 @@ end
 set :gems_start_port, 9001
 set :gems_count, 3
 
-
- 
-
-###################################################################################################
-# No need to edit this file below here (except nerds, of course)
-#
-
-# Some helper variables
-set :path_application, "#{path_base}/applications/#{application}"
-set :path_data, "#{path_application}/data"
-set :gem_ports, (gems_start_port..(gems_start_port + gems_count - 1)).to_a
-
-if not exists?(:stone)
-  set :stone, application
-end
-
-# set capistrano hostname for the "app" role
-role :app, host
-
-# Whether to use sudo for all shell commands
-set :use_sudo, false
-
-# Gemstone environment settings
-set :default_environment, {
-  'GEMSTONE' => path_gemstone,                                        # Path to the Gemstone product directory
-  'GEMSTONE_USER' => user,                                            # Linux username of GemStone administrator
-  'GEMSTONE_LOGDIR' => path_data,                                     # Log dir
-  'GEMSTONE_NAME' => stone,                                           # Stone name
-  'GEMSTONE_DATADIR' => path_data,                                    # Data dir
-  'GEMSTONE_KEYFILE' => "#{path_seaside}/etc/gemstone.key",           # Path to the Gemstone Web Edition keyfile (Same for all applications!)
-  'GEMSTONE_SYS_CONF' => "#{path_seaside}/data/system.conf",          # Path to Gemstone system config file (Same for all applications!)
-  'GEMSTONE_EXE_CONF' => "#{path_seaside}/data",                      # Path to Gemstone executable config directory
-  'PATH' => "#{path_gemstone}/bin:#{path_seaside}/bin:$PATH",         # Path to Gemstone binaries
-  'LD_LIBRARY_PATH' => "#{path_gemstone}/lib:$LD_LIBRARY_PATH",       # Gemstone library path
-  'DYLD_LIBRARY_PATH' => "#{path_gemstone}/lib:$DYLD_LIBRARY_PATH",   # MacOSX library path
-  'GEMS' => gem_ports.join(' ')                                       # Listing of the local ports, each running one application instance gem
-}
+# Configure your seaside entry points here, as a hash with {YourRootComponent => 'entry-point-name', ...}
+set :entry_points, {'VGApplicationComponent' => 'vegl'}
